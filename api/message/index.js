a26bf505f71a8a7cf4e6cc42a83c448ecc9ecf37
Thabiso Mahlaola <2426592@students.wits.ac.za>
2024-08-08 22:01:07 +0200

    sample test

--- api/message/index.js
+++ api/message/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
+    const executionId = context.invocationId;
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
@@ -9,7 +9,8 @@ module.exports = async function (context, req) {
         context.res = {
             status: 200,
             body: {
-                text: "Hello Vin, You are better than all of them"
+                text: "Hello Vin, You are better than all of them",
+                executionId: executionId
             }
         };
     
